d26d27bca1af65cf872abae18b62757220806b39
marcocareddu <saldumaccu@tuta.io>
2023-09-11 16:53:20 +0200

    Added Paginate to Api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(5);
         return response()->json($projects);
     }
 

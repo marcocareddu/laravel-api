4ceaac6acda43d2053be8a7f5a5a6f246689fe58
marcocareddu <saldumaccu@tuta.io>
2023-09-13 15:45:31 +0200

    Added mail send

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\MessageMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
     public function mailMessage()
     {
-        return response()->json('ciao');
+        $mail = new MessageMail();
+        Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+
+        return response(null, 204);
     }
 }

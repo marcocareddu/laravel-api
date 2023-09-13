d4545e222a15a754853214ba2077bb331a51af61
marcocareddu <saldumaccu@tuta.io>
2023-09-13 17:21:26 +0200

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST'],
 
     'allowed_origins' => [env('APP_FRONTEND_URL', 'http://localhost:5174')],
 

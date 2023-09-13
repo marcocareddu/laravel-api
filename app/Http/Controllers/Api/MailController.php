52cad6eec7d4aff598d1261cfa1cfb81e91cd4be
marcocareddu <saldumaccu@tuta.io>
2023-09-13 16:31:47 +0200

    Added Validator

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\MessageMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class MailController extends Controller
 {
@@ -13,6 +14,29 @@ class MailController extends Controller
     {
 
         $data = $request->all();
+
+        // Validation
+        $validator = Validator::make(
+            $data,
+            [
+                'email' => 'required|email',
+                'subject' => 'required|string',
+                'content' => 'required|string',
+            ],
+            [
+                'email.required' => 'Devi inserire una mail',
+                'email.email' => 'La mail non è valida',
+                'subject.required' => "L'oggetto è obbligatorio",
+                'content.required' => "Devi inserire il contenuto"
+            ]
+        );
+
+
+        // If validation fails, return errors bag & conver into object
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
         $mail = new MessageMail(
             sender: $data['mail'],
             subject: $data['subject'],
